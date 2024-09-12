c63c25aba5ba990d141295196b36639b056287dd
Simon Meers <simon@simonmeers.com>
2024-09-12 14:43:30 +1000

    try fetch()ing for deeper validation

--- oembed/fields.py
+++ oembed/fields.py
@@ -1,8 +1,10 @@
 import re
+from urllib2 import HTTPError
 
 from django.core import exceptions
 from django.db import models
 
+from .core import build_url, fetch, MAX_WIDTH, MAX_HEIGHT
 from .models import ProviderRule
 
 class OEmbedField(models.URLField):
@@ -17,6 +19,25 @@ class OEmbedField(models.URLField):
     def validate(self, value, model_instance):
         for pattern in ProviderRule.objects.values_list('regex', flat=True):
             if re.match(pattern, value):
+                rule = ProviderRule.objects.filter(regex=pattern).first()
+                url = build_url(rule.endpoint, value, MAX_WIDTH, MAX_HEIGHT)
+                try:
+                    fetch(url)
+                except HTTPError as e:
+                    if e.code == 401:
+                        raise exceptions.ValidationError(
+                            u'Please ensure the settings for this video allow '
+                            u'embedding in external sites ({})'.format(
+                                e
+                            )
+                        )
+                    raise exceptions.ValidationError(
+                        u'Cannot embed video: {}'.format(e)
+                    )
+                except Exception as e:
+                    raise exceptions.ValidationError(
+                        u'Cannot embed video: {}'.format(e)
+                    )
                 return
         raise exceptions.ValidationError('Not a valid oEmbed link')
 
